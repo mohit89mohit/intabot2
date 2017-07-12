602b0d0f9cc0b8739eb65a22d00d1d988c6cae3d
mohit89mohit <mohitchambial89mohit@gmail.com>
2017-07-11 22:33:44 -0700

    my 2nd project on instagram

---
+++ constants.py
@@ -0,0 +1,6 @@
+# global variables file
+
+# instagram API access token
+APP_ACCESS_TOKEN = '2039341033.c4759ec.9a6e0b9ff1e3440cac459c93db64b730'
+# instagram API base url
+BASE_URL = 'https://api.instagram.com/v1/'
